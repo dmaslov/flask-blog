ffb1098d3c184a61b154542ea8022bb0bdde9262
Dmitry Maslov <maslov.dmitrij@gmail.com>
2013-11-07 00:12:40 +0200

    added lightbox2 query plugin to “single post” and “post preview” pages added resize article image on index page, single post page and post preview page

--- static/js/main.js
+++ static/js/main.js
@@ -14,6 +14,11 @@ $(function(){
     }
 
     $('a[data-target="_blank"]').attr('target', '_blank');
+    $('.post .no-lightbox img').addClass('col-lg-12');
+    $('.post .article img').each(function(index, el){
+        var anchor = '<a href="'+$(el).attr('src')+'" title="'+$(el).attr('alt')+'" data-lightbox="'+$(el).attr('src')+'"><img src="'+$(el).attr('src')+'" alt="'+$(el).attr('alt')+'" class="col-lg-12"></a>';
+        $(el).replaceWith(anchor);
+    });
     $('a.icon').on('click', function(){
         return confirm('Are you shure?');
     });
