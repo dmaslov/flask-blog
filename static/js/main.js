a84ecbc84bde4c7db1ea81567f7c43aa11084614
Dmitry Maslov <maslov.dmitrij@gmail.com>
2013-11-28 23:32:19 +0200

    small refactoring

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,16 @@
 window.MDM_SILENT = true;
 $(function(){
-    var documentWidth = $(document).width();
+    $('a[data-target="_blank"]').attr('target', '_blank');
+
+    if($('#post-short').length && $('#post-full').length) {
+        $('#post-short').mdmagick();
+        $('#post-full').mdmagick();
+    }
+
+    $('.post .article img').each(function(index, el){
+        var anchor = '<a href="'+$(el).attr('src')+'" title="'+$(el).attr('alt')+'" data-lightbox="'+$(el).attr('src')+'"><img src="'+$(el).attr('src')+'" alt="'+$(el).attr('alt')+'"></a>';
+        $(el).replaceWith(anchor);
+    });
 
     $('#post-preview').on('click', function(){
         var postForm = $('#post-form');
@@ -9,29 +19,6 @@ $(function(){
     $('#post-submit').on('click', function(){
         var postForm = $('#post-form');
         postForm.find('#preview').val('');
-    });
-    if($('#post-short').length && $('#post-full').length) {
-        $('#post-short').mdmagick();
-        $('#post-full').mdmagick();
-    }
-
-    $('a[data-target="_blank"]').attr('target', '_blank');
-
-    $('.post .no-lightbox img').each(function(){
-        var imgWidth = $(this).width();
-        if((imgWidth * 100) / documentWidth > 33) {
-            $(this).addClass('col-lg-12');
-        }
-    });
-
-    $('.post .article img').each(function(index, el){
-        var imgWidth = $(this).width();
-        var className = '';
-        if((imgWidth * 100) / documentWidth > 33) {
-            className = 'col-lg-12';
-        }
-        var anchor = '<a href="'+$(el).attr('src')+'" title="'+$(el).attr('alt')+'" data-lightbox="'+$(el).attr('src')+'"><img src="'+$(el).attr('src')+'" alt="'+$(el).attr('alt')+'" class="'+className+'"></a>';
-        $(el).replaceWith(anchor);
     });
     $('a.icon').on('click', function(){
         return confirm('Are you shure?');
