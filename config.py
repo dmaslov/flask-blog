30af0ecf8dfd4a21cdaac22ecd1229062d47ed80
def <srbhatia10@gmail.com>
2014-06-12 09:08:43 +0530

    Generate SECRET_KEY randomly & edit .gitignore to hide it

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import pymongo
+import os
 
 CONNECTION_STRING = "mongodb://localhost"  # replace it with your settings
 CONNECTION = pymongo.MongoClient(CONNECTION_STRING)
@@ -9,7 +10,27 @@ POSTS_COLLECTION = DATABASE.posts
 USERS_COLLECTION = DATABASE.users
 SETTINGS_COLLECTION = DATABASE.settings
 
-SECRET_KEY = "Howdy, cowboy!"  # need to replace with some sequence..
+SECRET_KEY = ""
+basedir = os.path.abspath(os.path.dirname(__file__))
+secret_file = os.path.join(basedir, '.secret')
+if os.path.exists(secret_file):
+    # Read SECRET_KEY from .secret file
+    f = open(secret_file, 'r')
+    SECRET_KEY = f.read().strip()
+    f.close()
+else:
+    # Generate SECRET_KEY & save it away
+    SECRET_KEY = os.urandom(24)
+    f = open(secret_file, 'w')
+    f.write(SECRET_KEY)
+    f.close()
+    # Modeify .gitignore to include .secret file
+    gitignore_file = os.path.join(basedir, '.gitignore')
+    f = open(gitignore_file, 'a+')
+    if '.secret' not in f.readlines() and '.secret\n' not in f.readlines():
+        f.write('.secret\n')
+    f.close()
+
 LOG_FILE = "app.log"
 
 DEBUG = True  # set it to False on production
